71d235334d65186bfe678c5268f85bf518075bef
Abdullah Taufeeq <m.abdullahtaufeeq@ggmail.com>
2025-02-24 16:09:44 +0500

    assignment 2

---
+++ script.js
@@ -0,0 +1,76 @@
+// var num = prompt("Enter a number:");
+// if (num > 0) document.write("Positive<br>");
+// else if (num < 0) document.write("Negative<br>");
+// else document.write("Zero<br>");
+
+// --------------------------------------------
+
+// var num2 = prompt("Enter a number:");
+
+// if (num2 % 2 === 0) document.write("Even<br>");
+
+// else document.write("Odd<br>");
+
+// --------------------------------------------
+
+// var age = prompt("Enter your age:");
+
+// if (age >= 18) document.write("Eligible to vote<br>");
+
+// else document.write("Not eligible<br>");
+
+// --------------------------------------------
+
+// var a = prompt("Enter first number:");
+
+// var b = prompt("Enter second number:");
+
+// if (a > b) document.write("Largest: " + a + "<br>");
+
+// else document.write("Largest: " + b + "<br>");
+
+// --------------------------------------------
+
+// var age = prompt("Enter your age:");
+
+// var hasCard = confirm("Do you have a membership card? Click OK for Yes, Cancel for No.");
+
+// if (age >= 60 || hasCard) document.write("Eligible for discount<br>");
+
+// else document.write("Not eligible<br>");
+
+// --------------------------------------------
+
+// var username = prompt("Enter username:");
+
+// var password = prompt("Enter password:");
+
+// if (username === "admin" && password === "12345") document.write("Login successful<br>");
+
+// else document.write("Invalid credentials<br>");
+
+// --------------------------------------------
+
+// var marks = prompt("Enter your marks:");
+
+// if (marks >= 90) document.write("Grade: A<br>");
+
+// else if (marks >= 80) document.write("Grade: B<br>");
+
+// else if (marks >= 70) document.write("Grade: C<br>");
+
+// else if (marks >= 60) document.write("Grade: D<br>");
+
+// else document.write("Grade: F<br>");
+
+// --------------------------------------------
+
+// var color = prompt("Enter traffic light color (red, yellow, green):").toLowerCase();
+
+// if (color === "red") document.write("Stop<br>");
+
+// else if (color === "yellow") document.write("Slow down<br>");
+
+// else if (color === "green") document.write("Go<br>");
+
+// else document.write("Invalid color<br>");
